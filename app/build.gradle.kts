39bf18506cecaaf7bc5f359896dfc4378a1c89ae
ALBERTO YAEL REYES HERNANDEZ <118036132+ALBERTOYAELREYESHERNANDEZ@users.noreply.github.com>
2026-02-26 09:37:35 -0600

    JuevesCommit
    Se hizo un commit hoy 26 de febrero

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
