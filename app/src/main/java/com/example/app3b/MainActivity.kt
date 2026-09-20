00307806dc8110ec9f23c787a5da370d808214eb
ALBERTO YAEL REYES HERNANDEZ <118036132+ALBERTOYAELREYESHERNANDEZ@users.noreply.github.com>
2026-09-19 23:37:58 -0600

    CommitAndorid

--- app/src/main/java/com/example/app3b/MainActivity.kt
+++ app/src/main/java/com/example/app3b/MainActivity.kt
@@ -12,10 +12,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+            .findFragmentById(R.id.nav_host_fragment) as? NavHostFragment
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        bottomNavigationView.setupWithNavController(navController)
+        if (navHostFragment != null) {
+            val navController = navHostFragment.navController
+            val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+            bottomNavigationView?.setupWithNavController(navController)
+        }
     }
 }
